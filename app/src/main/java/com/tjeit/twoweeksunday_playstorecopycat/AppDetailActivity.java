befc21655f1daa7b1f7ec2d52987160abc3300ce
yhkim <kvoyant@gmail.com>
2019-04-27 11:26:22 +0900

    Calener를 만들어서 년월일가져온 값으로 세팅

--- app/src/main/java/com/tjeit/twoweeksunday_playstorecopycat/AppDetailActivity.java
+++ app/src/main/java/com/tjeit/twoweeksunday_playstorecopycat/AppDetailActivity.java
@@ -1,14 +1,20 @@
 package com.tjeit.twoweeksunday_playstorecopycat;
 
+import android.app.DatePickerDialog;
 import android.content.Intent;
 import android.databinding.DataBindingUtil;
 import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.DatePicker;
+import android.widget.Toast;
 
 import com.tjeit.twoweeksunday_playstorecopycat.databinding.ActivityAppDetailBinding;
 import com.tjeit.twoweeksunday_playstorecopycat.datas.App;
+
+import java.util.Calendar;
 
 public class AppDetailActivity extends AppCompatActivity {
 
@@ -53,6 +59,37 @@ public class AppDetailActivity extends AppCompatActivity {
                 Uri phoneUri = Uri.parse("tel:010-1234-5678");
                 Intent intent = new Intent(Intent.ACTION_DIAL, phoneUri);
                 startActivity(intent);
+            }
+        });
+
+        act.dateTxt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d("텍스트뷰클릭!", "실제로 동작하나?");
+
+                DatePickerDialog dpd = new DatePickerDialog(AppDetailActivity.this, new DatePickerDialog.OnDateSetListener() {
+                    @Override
+                    public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+//                        Toast.makeText(AppDetailActivity.this, String.format("%d년 %d월 %d일",year,month,dayOfMonth),Toast.LENGTH_SHORT).show();
+
+                        Calendar cal = Calendar.getInstance(); //new Calendar라고 만들지 않는다.
+
+                        // 1. 항목별로 어떤값을 갖게 할건지? 코딩 방식
+                        cal.set(Calendar.YEAR, year);
+                        cal.set(Calendar.MONTH, month);
+                        cal.set(Calendar.DAY_OF_MONTH, dayOfMonth);
+
+                        //2. 년/월/일 한꺼번에 세팅
+                        cal.set(year, month , dayOfMonth);
+
+                        //같은 메쏘드인데 , arg의 종류/갯수에 따라 다른 행동을 함. => overloading 의 예시.
+
+
+
+
+                    }
+                }, 2019,3,27);
+                dpd.show();
             }
         });
     }
